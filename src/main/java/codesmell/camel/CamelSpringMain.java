40c988339a2494508100cf85b5d546cecc9e0380
codesmell <mbarlotta@gmail.com>
2022-07-08 16:12:38 -0400

    replace java-config w/ spring.Main & XML

--- src/main/java/codesmell/camel/CamelSpringMain.java
+++ src/main/java/codesmell/camel/CamelSpringMain.java
@@ -1,18 +1,21 @@
 package codesmell.camel;
 
 import codesmell.util.LogFormatUtil;
-import org.apache.camel.spring.javaconfig.Main;
+import org.apache.camel.spring.Main;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.event.Level;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class CamelSpringMain {
     private static final Logger LOGGER = LoggerFactory.getLogger(CamelSpringMain.class);
 
     public static void main(String[] args) throws Exception {
         LogFormatUtil.logger(LOGGER)
-            .withMessage("camel spring main with java config...")
-            .andLog(Level.INFO);
+                .withMessage("camel spring main with java config...")
+                .andLog(Level.INFO);
 
         boot();
     }
@@ -21,8 +24,9 @@ public class CamelSpringMain {
      * Camel Spring Main (Java-Config)
      */
     public static void boot() throws Exception {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("META-INF/spring/beans.xml");
         Main camelSpringMain = new Main();
-        camelSpringMain.setBasedPackages("codesmell.config");
+        camelSpringMain.setApplicationContext((AbstractApplicationContext) ctx);
         camelSpringMain.run();
     }
 }
